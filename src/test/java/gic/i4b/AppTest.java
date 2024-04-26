92eb7069ccc887d5fdeb90bb7adda9fb2daf41b6
selytheng <kh.lytheng@gmail.com>
2024-04-26 16:13:45 +0700

    add test to maven.yml

--- src/test/java/gic/i4b/AppTest.java
+++ src/test/java/gic/i4b/AppTest.java
@@ -1,5 +1,6 @@
 package gic.i4b;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,13 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+        // assertEquals(2, 3);
     }
 }
